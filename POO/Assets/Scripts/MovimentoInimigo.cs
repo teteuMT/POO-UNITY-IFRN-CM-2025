7622f552b5eb9c5eb5a3cdbf2906fa6c1b27e2c1
Saulo Daniel Ferreira Pontes <saulodfp@gmail.com>
2025-05-12 16:32:30 -0300

    Adicionado ajuste de movimento.

--- POO/Assets/Scripts/MovimentoInimigo.cs
+++ POO/Assets/Scripts/MovimentoInimigo.cs
@@ -1,3 +1,5 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MovimentoInimigo : MonoBehaviour
@@ -7,12 +9,16 @@ public class MovimentoInimigo : MonoBehaviour
     private Rigidbody _rigidbody;
     private float velocidade;
 
-    public float raioDeVisao = 3;
+    public float raioDeVisao = 10;
+    private bool naVisao = false;
+    
+    private SphereCollider _sphereCollider;
     
     void Start()
     {
         _rigidbody = gameObject.GetComponent<Rigidbody>();
         velocidade = gameObject.GetComponent<Inimigo>().Velocidade();
+        _sphereCollider = gameObject.GetComponent<SphereCollider>();
         
         _player = GameObject.FindWithTag("Player");
     }
@@ -20,12 +26,33 @@ public class MovimentoInimigo : MonoBehaviour
  
     void Update()
     {
-        if (Vector3.Distance(transform.position, _player.transform.position) < raioDeVisao )
-        {
+        _sphereCollider.radius = raioDeVisao;
+        
+       // if (Vector3.Distance(transform.position, _player.transform.position) < raioDeVisao )
+       if(naVisao == true) 
+       {
             transform.LookAt(_player.transform.position);
             transform.position = Vector3.MoveTowards(transform.position, 
                 _player.transform.position, 
-                velocidade * Time.deltaTime);
+                velocidade * Time.deltaTime); 
+        } 
+        // Debug.DrawLine(transform.position, _player.transform.position, Color.red);
+        
+    }
+    void OnTriggerStay(Collider colisao)
+    {
+        if (colisao.gameObject.CompareTag("Player"))
+        {
+            naVisao = true;
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider colisao)
+    {
+        if (colisao.gameObject.CompareTag("Player"))
+        {
+            naVisao = false;
         }
     }
 }
